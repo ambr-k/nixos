17e61242004764b7357a61277faa84e947e091c3
akirschbaum <akirschbaum@blueorigin.com>
2025-11-21 17:06:22 +0000

    yazi plugins

--- home/modules/zsh/yazi.nix
+++ home/modules/zsh/yazi.nix
@@ -1,12 +1,48 @@
 {pkgs, ...}: {
-  programs.yazi.enable = true;
-  programs.yazi.theme.flavor.dark = "rose-pine-moon";
-  programs.yazi.flavors.rose-pine-moon = let
-    repo = pkgs.fetchFromGitHub {
-      owner = "rose-pine";
-      repo = "yazi";
-      rev = "fd385266af5f3419657e449607f3e87f062d0d2e";
-      sha256 = "3j7TTtzG+GCB4uVeCiuvb/0dCkHPz7X+MDBVVUp646A=";
+  programs.yazi = {
+    enable = true;
+
+    theme.flavor.dark = "rose-pine-moon";
+    flavors.rose-pine-moon = let
+      repo = pkgs.fetchFromGitHub {
+        owner = "rose-pine";
+        repo = "yazi";
+        rev = "fd385266af5f3419657e449607f3e87f062d0d2e";
+        sha256 = "3j7TTtzG+GCB4uVeCiuvb/0dCkHPz7X+MDBVVUp646A=";
+      };
+    in "${repo}/flavors/rose-pine-moon.yazi";
+
+    plugins = {
+      chmod = pkgs.yaziPlugins.chmod;
+      git = pkgs.yaziPlugins.git;
+      lazygit = pkgs.yaziPlugins.lazygit;
     };
-  in "${repo}/flavors/rose-pine-moon.yazi";
+    keymap.mgr.prepend_keymap = [
+      {
+        on = ["c" "m"];
+        run = "plugin chmod";
+        desc = "chmod selected files";
+      }
+      {
+        on = ["g" "i"];
+        run = "plugin lazygit";
+        desc = "Run lazyzgit";
+      }
+    ];
+    settings.plugin.prepend_fetchers = [
+      {
+        id = "git";
+        name = "*";
+        run = "git";
+      }
+      {
+        id = "git";
+        name = "*/";
+        run = "git";
+      }
+    ];
+    initLua = ''
+      require("git"):setup()
+    '';
+  };
 }
