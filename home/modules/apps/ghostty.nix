083486ae9ee1cb1b461c1dff2f16e17d7372119f
akirschbaum <akirschbaum@blueorigin.com>
2025-12-12 20:42:39 +0000

    ghostty quick term

--- home/modules/apps/ghostty.nix
+++ home/modules/apps/ghostty.nix
@@ -1,12 +1,17 @@
 {...}: {
   programs.ghostty = {
     enable = true;
+
     settings = {
       font-family = "AporeticSerifMono Nerd Font";
       font-size = 14;
       window-padding-x = 5;
       window-padding-y = 5;
       window-padding-balance = true;
+
+      keybind = "global:super+tab=toggle_quick_terminal";
+      quick-terminal-position = "right";
+      quick-terminal-size = "25%";
     };
   };
 }
