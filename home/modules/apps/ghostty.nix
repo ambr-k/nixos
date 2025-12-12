e46c36cf2ebbda55dd956816fd8892be1c9ed02e
Amber Kirschbaum <amber.fae.k@gmail.com>
2025-12-12 13:45:28 -0700

    Revert "ghostty quick term"
    This reverts commit 083486ae9ee1cb1b461c1dff2f16e17d7372119f.

--- home/modules/apps/ghostty.nix
+++ home/modules/apps/ghostty.nix
@@ -1,17 +1,12 @@
 {...}: {
   programs.ghostty = {
     enable = true;
-
     settings = {
       font-family = "AporeticSerifMono Nerd Font";
       font-size = 14;
       window-padding-x = 5;
       window-padding-y = 5;
       window-padding-balance = true;
-
-      keybind = "global:super+tab=toggle_quick_terminal";
-      quick-terminal-position = "right";
-      quick-terminal-size = "25%";
     };
   };
 }
