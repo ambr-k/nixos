db1848947cd94f86c7726a858bfc52bd3b51485f
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-01-03 13:05:32 -0700

    openrgb

--- hosts/carbon/configuration.nix
+++ hosts/carbon/configuration.nix
@@ -1,6 +1,8 @@
 {
   config,
   pkgs,
+  pkgs-unstable,
+  lib,
   inputs,
   system,
   ...
@@ -11,7 +13,7 @@
     extraModulePackages = with config.boot.kernelPackages; [
       r8125
     ];
-    kernelModules = ["r8125"];
+    kernelModules = ["r8125" "i2c-dev"];
 
     supportedFilesystems = ["ntfs"];
   };
@@ -23,6 +25,31 @@
 
     inputs.aporetic-nerd-font.packages.${system}.aporetic-nerd-font
   ];
+
+  hardware.i2c.enable = true;
+  services.hardware.openrgb = {
+    enable = true;
+    package =
+      pkgs-unstable.openrgb-with-all-plugins.overrideAttrs
+      (old: {
+        src = pkgs.fetchFromGitLab {
+          owner = "CalcProgrammer1";
+          repo = "OpenRGB";
+          rev = "release_candidate_1.0rc2";
+          sha256 = "sha256-vdIA9i1ewcrfX5U7FkcRR+ISdH5uRi9fz9YU5IkPKJQ=";
+        };
+        patches = [
+          ./remove_openrgb_systemd.patch
+        ];
+        postPatch = ''
+          patchShebangs scripts/build-udev-rules.sh
+          substituteInPlace scripts/build-udev-rules.sh \
+           --replace-fail /usr/bin/env "${pkgs.coreutils}/bin/env"
+        '';
+        version = "release_candidate_1.0rc2";
+        motherboard = "intel";
+      });
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
