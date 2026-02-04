d15495649d8551c5fe35437a55f77cef1db78561
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-02-04 16:25:37 -0700

    feishin

--- home/modules/apps/music_library.nix
+++ home/modules/apps/music_library.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [beets nicotine-plus kew recordbox];
+  home.packages = with pkgs; [beets nicotine-plus kew recordbox feishin];
   xdg.configFile."beets/config.yaml".text = ''
     plugins: fetchart embedart edit discogs
   '';
