e8c67145e47965630af1ca294b293dc9f65e2334
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-02-16 11:23:00 -0700

    hyprland configs

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -22,6 +22,9 @@
       decoration.rounding = "8";
       decoration.blur.passes = 2;
       decoration.blur.special = true;
+      misc.enable_swallow = true;
+      misc.swallow_regex = "com\.mitchellh\.ghostty";
+      misc.focus_on_activate = true;
       bind = [
         "SUPER,Return,exec,ghostty"
         "SUPER CTRL,Return,exec,[float] ghostty"
