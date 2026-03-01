29d69030a2772aac507849bf57453d035a8cef78
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-02-28 18:40:29 -0700

    clipboard only screenshots

--- home/modules/hyprland/hyprshot.nix
+++ home/modules/hyprland/hyprshot.nix
@@ -2,6 +2,8 @@
   home.packages = [pkgs.hyprshot];
   wayland.windowManager.hyprland.settings.bind = [
     "SUPER,S,exec,hyprshot -m region"
+    "SUPER SHIFT,S,exec,hyprshot -m region --clipboard-only"
     "SUPER CTRL,S,exec,hyprshot -m window"
+    "SUPER CTRL SHIFT,S,exec,hyprshot -m window --clipboard-only"
   ];
 }
