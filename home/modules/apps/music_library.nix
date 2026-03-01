70aae0cdc372434fbcd377316a8960b853f7a6aa
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-02-28 18:40:00 -0700

    beets lyrics

--- home/modules/apps/music_library.nix
+++ home/modules/apps/music_library.nix
@@ -1,7 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [beets nicotine-plus kew recordbox feishin picard];
   xdg.configFile."beets/config.yaml".text = ''
-    plugins: fetchart embedart edit discogs
+    plugins: fetchart embedart edit discogs lyrics
+
+    lyrics:
+      synced: yes
+      force: yes
   '';
 
   wayland.windowManager.hyprland.settings.exec-once = ["[workspace special silent] feishin"];
