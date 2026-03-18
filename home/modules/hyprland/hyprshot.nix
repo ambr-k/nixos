d48324eecd9eb833788bc01d02ecff0e6508ebb7
Amber Kirschbaum <amber.fae.k@gmail.com>
2026-03-18 16:15:02 -0600

    improve screenshots

--- home/modules/hyprland/hyprshot.nix
+++ home/modules/hyprland/hyprshot.nix
@@ -1,9 +1,15 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.hyprshot];
+  home.packages = with pkgs; [hyprshot grim slurp swappy wl-clipboard];
   wayland.windowManager.hyprland.settings.bind = [
-    "SUPER,S,exec,hyprshot -m region"
-    "SUPER SHIFT,S,exec,hyprshot -m region --clipboard-only"
+    "SUPER,S,exec, grim -g \"$(slurp)\" - | swappy -f -"
+    "SUPER SHIFT,S,exec, grim -g \"$(slurp)\" - | wl-copy"
     "SUPER CTRL,S,exec,hyprshot -m window"
     "SUPER CTRL SHIFT,S,exec,hyprshot -m window --clipboard-only"
   ];
+
+  xdg.configFile."swappy/config".text = ''
+    [Default]
+    save_dir=$HOME/Pictures/Screenshots
+    early_exit=true
+  '';
 }
